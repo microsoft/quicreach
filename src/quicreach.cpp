4bdbaa6fba49a37bfcb1061d0bae3daac5454fb8
Nick Banks <nibanks@microsoft.com>
2022-04-03 13:35:15 -0400

    More stuff

--- src/quicreach.cpp
+++ src/quicreach.cpp
@@ -8,6 +8,9 @@
 #include <msquic.hpp>
 #include <stdio.h>
 #include <thread>
+#include <vector>
+
+#include "top_domains.h"
 
 #ifdef _WIN32
 #define QUIC_CALL __cdecl
@@ -15,12 +18,14 @@
 #define QUIC_CALL
 #endif
 
+using namespace std;
+
 const MsQuicApi* MsQuic;
 
 struct ReachConfig {
-    const char* ServerName {nullptr};
+    std::vector<const char*> HostNames;
     uint16_t Port {443};
-    MsQuicAlpn Alpns {"h3", "smb"};
+    MsQuicAlpn Alpn {"h3"};
     MsQuicSettings Settings;
     QUIC_CREDENTIAL_FLAGS CredFlags {QUIC_CREDENTIAL_FLAG_CLIENT};
 
@@ -31,25 +36,53 @@ struct ReachConfig {
 
 bool ParseConfig(int argc, char **argv, ReachConfig& Config) {
     if (argc < 2 || (!strcmp(argv[1], "?") || !strcmp(argv[1], "help"))) {
-        printf("Usage: quicreach <server> [options]\n");
+        printf("Usage: quicreach <hostname(s)> [options]\n");
         return false;
     }
-    Config.ServerName = argv[1];
+
+    // Parse hostname(s), treating '*' as all top-level domains.
+    if (!strcmp(argv[1], "*")) {
+        for (auto Domain : TopDomains) {
+            Config.HostNames.push_back(Domain);
+        }
+    } else {
+        char* HostName = (char*)argv[1];
+        do {
+            char* End = strchr(HostName, ',');
+            if (End) *End = 0;
+            Config.HostNames.push_back(HostName);
+            if (!End) break;
+            HostName = End + 1;
+        } while (true);
+    }
+
     return true;
 }
 
 bool TestReachability(const ReachConfig& Config) {
     MsQuicRegistration Registration("quicreach");
-    MsQuicConfiguration Configuration(Registration, Config.Alpns, Config.Settings, MsQuicCredentialConfig(Config.CredFlags));
-    MsQuicConnection Connection(Registration);
-    if (!Connection.IsValid()) { printf("Connection initializtion failed!\n"); return false; }
+    MsQuicConfiguration Configuration(Registration, Config.Alpn, Config.Settings, MsQuicCredentialConfig(Config.CredFlags));
+    if (!Configuration.IsValid()) { printf("Configuration initializtion failed!\n"); return false; }
 
-    if (QUIC_FAILED(Connection.Start(Configuration, Config.ServerName, Config.Port))) {
-        printf("Connection start failed!\n"); return false;
+    uint32_t ReachableCount = 0;
+    for (auto HostName : Config.HostNames) {
+        MsQuicConnection Connection(Registration);
+        if (!Connection.IsValid()) { printf("Connection initializtion failed!\n"); return false; }
+        if (QUIC_FAILED(Connection.Start(Configuration, HostName, Config.Port))) {
+            printf("Connection start failed!\n"); return false;
+        }
+
+        uint32_t tries = 0;
+        do {
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+            ++tries;
+        } while (!Connection.HandshakeComplete && tries < 50);
+
+        printf("%30s\t\t%s\n", HostName, Connection.HandshakeComplete ? "reachable" : "");
+        if (Connection.HandshakeComplete) ++ReachableCount;
     }
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    printf("Handshake complete = %s\n", Connection.HandshakeComplete ? "true" : "false");
+    if (ReachableCount > 1) printf("\n%u domains reachable\n", ReachableCount);
 
     return true;
 }
