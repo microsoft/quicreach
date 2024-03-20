570db01e396d47fc524a3b760d084a352dd85e48
Nick Banks <nibanks@microsoft.com>
2024-03-20 15:50:47 -0400

    Prepare for OneBranch Support (#202)

---
+++ .azure/post-build.ps1
@@ -0,0 +1,37 @@
+param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("x86", "x64", "arm", "arm64", "arm64ec")]
+    [string]$Arch = "x64",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Debug", "Release")]
+    [string]$Config = "Release"
+)
+
+Set-StrictMode -Version 'Latest'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+
+function Execute([String]$Name, [String]$Arguments) {
+    Write-Debug "$Name $Arguments"
+    $process = Start-Process $Name $Arguments -PassThru -NoNewWindow -WorkingDirectory "./build"
+    $handle = $process.Handle # Magic work around. Don't remove this line.
+    $process.WaitForExit();
+    if ($process.ExitCode -ne 0) {
+        Write-Error "$Name exited with status code $($process.ExitCode)"
+    }
+}
+
+if ($env:ONEBRANCH_ARCH) {
+    $Arch = $env:ONEBRANCH_ARCH
+}
+if ($env:ONEBRANCH_CONFIG) {
+    $Config = $env:ONEBRANCH_CONFIG
+}
+
+$platform = "$($Arch)fre"
+if ($Config -eq "Debug") {
+    $platform = "$($Arch)chk"
+}
+
+Execute 'C:/Program Files (x86)/WiX Toolset v3.14/bin/candle.exe' "../src/installer.wxs -o bin/$platform/quicreach.wixobj"
+Execute 'C:/Program Files (x86)/WiX Toolset v3.14/bin/light.exe' "-b bin/$($Arch)fre/Release -o bin/$platform/quicreach.msi bin/$($Arch)fre/quicreach.wixobj"
---
+++ .azure/pre-build.ps1
@@ -0,0 +1,49 @@
+param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("x86", "x64", "arm", "arm64", "arm64ec")]
+    [string]$Arch = "x64",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Debug", "Release")]
+    [string]$Config = "Release",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("schannel", "openssl")]
+    [string]$Tls = "schannel",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("static", "shared")]
+    [string]$Link = "static"
+)
+
+Set-StrictMode -Version 'Latest'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+
+function Execute([String]$Name, [String]$Arguments) {
+    Write-Debug "$Name $Arguments"
+    $process = Start-Process $Name $Arguments -PassThru -NoNewWindow -WorkingDirectory "./build"
+    $handle = $process.Handle # Magic work around. Don't remove this line.
+    $process.WaitForExit();
+    if ($process.ExitCode -ne 0) {
+        Write-Error "$Name exited with status code $($process.ExitCode)"
+    }
+}
+
+if ($env:ONEBRANCH_ARCH) {
+    $Arch = $env:ONEBRANCH_ARCH
+}
+if ($env:ONEBRANCH_CONFIG) {
+    $Config = $env:ONEBRANCH_CONFIG
+}
+
+if (!(Test-Path "./build")) {
+    New-Item -Path "./build" -ItemType Directory -Force | Out-Null
+}
+
+$_Arch = $Arch
+if ($_Arch -eq "x86") { $_Arch = "Win32" }
+
+$Shared = "off"
+if ($Link -ne "static") { $Shared = "on" }
+
+Execute "cmake" "-G ""Visual Studio 17 2022"" -A $_Arch -DREACH_ARCH=$_Arch -DQUIC_TLS=$Tls -DQUIC_BUILD_SHARED=$Shared .."
